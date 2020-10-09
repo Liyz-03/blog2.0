9b35150ce6ed8a09072e70eb155869f8deae9589
liyz <lyz1125548895@gmail.com>
2020-10-09 11:31:04 +0800

    新增过渡动画

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -13,18 +12,17 @@ import './css/global.css'
 // //引入组件
 import MarkdownItVue from 'markdown-it-vue'
 import 'markdown-it-vue/dist/markdown-it-vue.css'
+
 Vue.use(MarkdownItVue)
-
-
 
 //导入editor
 import Editor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+
 Vue.use(Editor)
 
 //导入element ui
-import './plugins/element';
-
+import './plugins/element'
 
 //注册滚动条
 import GeminiScrollbar from 'vue-gemini-scrollbar'
@@ -35,43 +33,36 @@ import 'nprogress/nprogress.css'
 import Axios from 'axios'
 
 import echarts from 'echarts'
-Vue.prototype.$echarts = echarts;
 
+Vue.prototype.$echarts = echarts
 
 Vue.use(Axios)
 // Axios.defaults.baseURL = 'http://192.168.43.228:8888';
 
 const axiosInstance = Axios.create({
-<<<<<<< HEAD
-    baseURL : 'http://106.54.224.61:8888',
-     // baseURL : 'http://123.57.41.144:8888',
+
+    baseURL: 'http://106.54.224.61:8888',
+    // baseURL : 'http://123.57.41.144:8888',
     // baseURL : 'http://127.0.0.1:8888',
-=======
-   
-    baseURL : 'http://127.0.0.1:8888',
->>>>>>> 4d6712695ce936826f62375813862f0ae294a54f
-});
-Vue.prototype.$http = axiosInstance;
+    // baseURL : 'http://127.0.0.1:8888',
+})
+Vue.prototype.$http = axiosInstance
 
 axiosInstance.interceptors.request.use(config => {
     // console.log(config);
     // config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
     nprogress.start()
-    config.headers.sessionToken = window.sessionStorage.getItem('token');
-    return config;
+    config.headers.sessionToken = window.sessionStorage.getItem('token')
+    return config
 })
-axiosInstance.interceptors.response.use(config=>{
+axiosInstance.interceptors.response.use(config => {
     nprogress.done()
     return config
 })
 
 Vue.config.productionTip = true
 
-
-
-
-
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
